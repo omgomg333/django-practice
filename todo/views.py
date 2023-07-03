84da354219fcf5309b61acc4bc16984d8c86c081
omgomg333 <skdydtjd12@gmail.com>
2023-07-04 06:02:55 +0900

    수정과 삭제기능 만들기

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,30 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 
 from todo.models import TodoList
 
 # Create your views here.
+
+def delete_todo(request, pk):
+	delTodo = get_object_or_404(TodoList, pk=pk)
+	delTodo.delete()
+	return redirect('todos')
+
+def update_todo(request, pk):
+	curTodo = get_object_or_404(TodoList, pk=pk)
+	if request.method == 'POST':
+		curTodo.todo = request.POST['todo']
+		curTodo.description = request.POST['description']
+		curTodo.important = request.POST.get('important') == "on"
+		curTodo.complete = request.POST.get('complete') == "on"
+		curTodo.save()
+		return redirect('todos')
+	return render(
+		request,
+		'todo/todo_update.html',
+		{
+			'curTodo': curTodo,
+		},
+	)
 
 def create_todo(request):
 	myTodo = TodoList()
